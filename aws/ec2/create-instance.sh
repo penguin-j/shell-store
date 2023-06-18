ded486ad100758fc625ae33a5c0e49de46e97ea1
j <penguinjinbei@gmail.com>
2023-06-18 20:14:25 +0900

    EC2作成時にVPCを指定できるよう変更

--- aws/ec2/create-instance.sh
+++ aws/ec2/create-instance.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 KEY_NAME=${1}
-SECURITY_GROUP_ID=${2}
+SUBNET_ID=${2}
+SECURITY_GROUP_ID=${3}
 
 aws ec2 run-instances \
     --image-id ami-0f9816f78187c68fb \
     --count 1 \
     --instance-type t2.micro \
     --key-name ${KEY_NAME} \
+    --subnet-id ${SUBNET_ID} \
     --security-group-ids ${SECURITY_GROUP_ID} \
     --associate-public-ip-address
--- aws/ec2/create-security-group.sh
+++ aws/ec2/create-security-group.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 SECURITY_GROUP_NAME=${1}
-DESCRIPTION=${2}
+VPC_ID=${2}
 
-aws ec2 create-security-group --group-name ${SECURITY_GROUP_NAME} --description ${DESCRIPTION}
+aws ec2 create-security-group \
+    --group-name ${SECURITY_GROUP_NAME} \
+    --description ${SECURITY_GROUP_NAME} \
+    --vpc-id ${VPC_ID}
