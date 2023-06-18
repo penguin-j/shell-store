4cc4fd1004f340fe038958f3c9eadb5b61f05fcf
j <penguinjinbei@gmail.com>
2023-06-18 18:45:52 +0900

    RDSインスタンス削除用スクリプト新規作成

--- aws/rds/delete-db-instance.sh
+++ aws/rds/delete-db-instance.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 INSTANCE_ID=${1}
-SECURITY_GROUP_ID=${2}
 DB_SUBNET_GROUP_NAME=${INSTANCE_ID}-subnet-group
 
-aws rds delete-db-instance \
+SECURITY_GROUP_ID=$(aws rds delete-db-instance \
     --db-instance-identifier ${INSTANCE_ID} \
-    --skip-final-snapshot
+    --skip-final-snapshot \
+    --query 'DBInstance.VpcSecurityGroups[0].VpcSecurityGroupId' \
+    --output text)
 
 # DBインスタンス削除が完了しないと次に進めないのでsleepする
 sleep 5m
