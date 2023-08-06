64636459b3527d868de597a4d1aaf6b042515ac1
j <penguinjinbei@gmail.com>
2023-08-06 22:55:21 +0900

    refactor

--- aws/sesv2/send-test-email.sh
+++ aws/sesv2/send-test-email.sh
@@ -6,4 +6,4 @@ TO_ADDRESSES=${2}
 aws sesv2 send-email \
     --from-email-address ${FROM_ADDRESS} \
     --destination ToAddresses=${TO_ADDRESSES} \
-    --content file://aws/sesv2/content.json
+    --content file://aws/sesv2/test-email-content.json
